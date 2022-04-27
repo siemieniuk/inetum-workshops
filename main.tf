43cce0592be5f7a6c6a5707d674d187f85b1203e
roertbb <robert.banaszak1997@gmail.com>
2022-04-27 19:14:40 +0200

    update formatting

--- main.tf
+++ main.tf
@@ -4,19 +4,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=2.84.0"
     }
- 
     random = {
       source  = "hashicorp/random"
       version = "3.1.2"
     }
- 
   }
- 
-  backend "azurerm" {
- 
-  }
+  backend "azurerm" {}
 }
- 
+
 provider "azurerm" {
   #   skip_provider_registration = true
   features {
@@ -25,11 +20,9 @@ provider "azurerm" {
     }
   }
 }
- 
+
 provider "random" {
   # Configuration options
 }
- 
-data "azurerm_client_config" "current" {}
- 
 
+data "azurerm_client_config" "current" {}
